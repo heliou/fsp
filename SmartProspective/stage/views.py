fb135ac6efd99a35863a315b50ce433d499c2469
sylvain.heliou <sylvain.heliou@smartprospective.com>
2016-11-02 17:01:35 +0100

    stage/variable now display a value from stage/views.py

--- SmartProspective/stage/views.py
+++ SmartProspective/stage/views.py
@@ -5,7 +5,8 @@ def basic(request):
     return HttpResponse("It's a basic page, in a basic stage")
 
 def variable(request):
-    return HttpResponse("It's a VAR page, in a VAR stage")
+    text_variable = "It's a var !"
+    return HttpResponse(text_variable)
 
 def default(request):
     return HttpResponse("Default display")
