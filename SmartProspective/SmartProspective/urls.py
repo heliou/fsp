258aec2063dc42bff59ea97156b7e9df49a58181
sylvain.heliou <sylvain.heliou@smartprospective.com>
2016-11-03 15:19:46 +0100

    create db & display on /admin

--- SmartProspective/SmartProspective/urls.py
+++ SmartProspective/SmartProspective/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+admin.autodiscover()
+
 
 urlpatterns = [
-    url(r'^stage/', admin.site.urls),
+    url(r'^stage/', include('stage.urls')),
     url(r'^admin/', admin.site.urls),
 ]
